14065edf42971310b3225921cf04dbd429e33040
Stuart Bates <stuart@builtbyclick.com>
2014-03-31 15:21:55 +0100

    Removed call to defunct load data method and refactored the overriding view

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,14 +1,12 @@
 Spree::Admin::ProductsController.class_eval do
 
   def highlight
-    load_object
     @product.highlight
     flash[:notice] = t('highlight_products.product_has_been_highlighted')
     redirect_to admin_products_url
   end
       
   def unhighlight
-    load_object
     @product.unhighlight
     flash[:notice] = t('highlight_products.product_has_been_unhighlighted')
     redirect_to admin_products_url
